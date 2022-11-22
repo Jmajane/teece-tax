84f9af592ea54d9c7cbc6285c756735a579f29b2
Jmajane <jordan.majane@outlook.com>
2022-11-22 11:08:55 -0500

    creating layout for Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,24 @@ const Home = () => {
       <br></br>
       <br></br>
         <h1>Home page</h1>
+
+        <div>
+          <h3>Services</h3>
+          <ul>
+            <li>Tax Preparation</li>
+            <li>Prior Years Tax Preparation</li>
+            <li>Multi-State Tax</li>
+            <li>Tax Planning</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
     </div>
   )
 }
