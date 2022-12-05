0b7ee5fbe6bee4fa865e2f8bab476070575ca2f0
Jmajane <jordan.majane@outlook.com>
2022-12-05 11:39:23 -0500

    contact us button added

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -6,6 +6,9 @@ const Header = () => {
     <section id='header'>       
         <div className='header'>
             <img className='header__image' src={TeeceTax} alt="company logo" />
+            <div className='button'>
+                <a href='/Contact' className='btn btn-primary'>Send your taxes our way</a>
+            </div>
         </div>
     </section>
   )
