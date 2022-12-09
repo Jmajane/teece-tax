dc1b5539742549d0f13a60522827f9deac9ae99c
Jmajane <jordan.majane@outlook.com>
2022-12-09 15:39:22 -0500

    completed personal taxes page, completed css for personal taxes

--- src/pages/personal/PersonalTaxes.jsx
+++ src/pages/personal/PersonalTaxes.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import './personalTaxes.css'
+import Footer from '../footer/Footer'
+
+import {GiCheckMark} from 'react-icons/gi'
+import {MdAttachMoney} from 'react-icons/md'
 
 const PersonalTaxes = () => {
   return (
-    <div>
-      <br></br>
-      <br></br>
-      <h1>Personal Taxes</h1>
-    </div>
+    <section id='personalTaxes'>
+      <div className='container personalTaxes__title'>
+        <h1>Personal Taxes</h1>
+      </div>
+
+      <div className='container personalTaxes__container'>
+        <article className='personalTaxes'>
+          <div className='personalTaxes__head'>
+            <h2>Personal Tax Returns</h2>
+          </div>
+
+          <ul className='personalTaxes__list'>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Federal Return</p>
+              <p><MdAttachMoney className='home__list-icon' />115</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per State Return</p>
+              <p><MdAttachMoney className='home__list-icon' />25</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per Schedule C Business</p>
+              <p><MdAttachMoney className='home__list-icon' />75</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per Schedule E Rental Property</p>
+              <p><MdAttachMoney className='home__list-icon' />75</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> 1099-B Stocks / Cyrpto</p>
+              <p><MdAttachMoney className='home__list-icon' />10</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+
+      <Footer />
+      
+    </section>
   )
 }
 
