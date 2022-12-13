dc8909057a48423aa95fbe2e05d472a5f1c8fdfe
Jmajane <jordan.majane@outlook.com>
2022-12-13 17:39:04 -0500

    creating small business / LLC page

--- src/pages/smallBusiness/SmallBusiness.jsx
+++ src/pages/smallBusiness/SmallBusiness.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import './smallBusiness.css'
 
-const SmallBusiness = () => {
+import Footer from '../footer/Footer'
+
+import {GiCheckMark} from 'react-icons/gi'
+import {MdAttachMoney} from 'react-icons/md'
+
+const Small = () => {
   return (
-    <div>
-      <br></br>
-      <br></br>
-      <h1>Small Business / LLC</h1>
-    </div>
+    <section id='smallBusiness'>
+      <div className='container smallBusiness__title'>
+        <h1>Personal Taxes</h1>
+      </div>
+
+      <div className='container smallBusiness__container'>
+        <article className='smallBusiness'>
+          <div className='smallBusiness__head'>
+            <h2>Personal Tax Returns</h2>
+          </div>
+
+          <ul className='smallBusiness__list'>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Federal Return</p>
+              <p><MdAttachMoney className='home__list-icon' />115</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per State Return</p>
+              <p><MdAttachMoney className='home__list-icon' />25</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per Schedule C Business</p>
+              <p><MdAttachMoney className='home__list-icon' />75</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> Per Schedule E Rental Property</p>
+              <p><MdAttachMoney className='home__list-icon' />75</p>
+            </li>
+            <li>
+              <p><GiCheckMark className='home__list-icon' /> 1099-B Stocks / Cyrpto</p>
+              <p><MdAttachMoney className='home__list-icon' />10</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+
+      <Footer />
+      
+    </section>
   )
 }
 
