b62ce360b5c5401651d8cb3c5215e71a4b34831a
Jmajane <jordan.majane@outlook.com>
2022-12-27 17:09:48 -0500

    added company photo back

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import "./header.css"
 
-// import TeeceTax from "../../assets/TeeceTax.png"
+import TeeceTax from "../../assets/TeeceTax.png"
 
 const Header = () => {
   return (
     <section id='header'>       
         <div className='header'>
-            <img className='header__image' src={""} alt="company logo" />
+            <img className='header__image' src={TeeceTax} alt="company logo" />
             <div className='button'>
                 <a href='/Contact' className='btn btn-primary'>Contact Us</a>
             </div>
