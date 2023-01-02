f524f24402d18b9c4abb30a15fb3bb13f8f0a831
Jmajane <jordan.majane@outlook.com>
2023-01-02 15:14:11 -0500

    updated footer to have facebook link

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import './footer.css'
 
+import {GrFacebook} from 'react-icons/gr'
+
 
 const Footer = () => {
   return (
@@ -12,6 +14,12 @@ const Footer = () => {
             <li><a href='/SmallBusiness'>Small Business</a></li>
             <li><a href='/Contact'>Contact</a></li>
             <li><a href="https://www.mytaxdocs.com/?o=19413" target='_blank' rel="noreferrer">Client Portal</a></li>
+        </ul>
+
+        <ul className='permalinks'>
+          <li>
+            <a href='https://www.facebook.com/TeeceTaxSolutions' target="_blank" rel="noreferrer"><GrFacebook className='footer__list-icon'/> Follow us on FaceBook</a>
+          </li>
         </ul>
 
         <div className='footer__copyright'>
